f384fbc03a228be83f6a29e5428e7790222c8765
Julian Kniephoff <me@juliankniephoff.com>
2016-01-15 21:36:38 +0100

    Fix handling of failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,4 +11,10 @@ class SessionsController < ApplicationController
     # TODO Redirect to where we came from
     redirect_to login_path
   end
+
+  def failure
+    # TODO Should we know about the env here?
+    @message = t(env['omniauth.error.type'], scope: :omniauth)
+    render :new
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,10 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   on_failure do |env|
     # TODO This does not belong here, logically
     # Shouldn't we use something like
-    #OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+    #     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
     # See also the routes.rb
-    flash = env[ActionDispatch::Flash::KEY] ||= ActionDispatch::Flash::FlashHash.new
-    flash.alert = I18n.t(env['omniauth.error.type'], scope: :omniauth)
-    SessionsController.action(:new).call(env)
+    SessionsController.action(:failure).call(env)
   end
 end
