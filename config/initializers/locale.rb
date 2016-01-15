4c10abe7f8f7f439514def5a6ea3121512fa7eff
Julian Kniephoff <me@juliankniephoff.com>
2016-01-15 11:13:01 +0100

    Set up error handling for login

---
+++ config/initializers/locale.rb
@@ -0,0 +1,5 @@
+# TODO This should not be necessary
+# See https://github.com/svenfuchs/i18n/issues/224
+I18n.enforce_available_locales = false
+
+I18n.default_locale = :de
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identity
+
+  on_failure do |env|
+    # TODO This does not belong here, logically
+    # Shouldn't we use something like
+    #OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+    # See also the routes.rb
+    flash = env[ActionDispatch::Flash::KEY] ||= ActionDispatch::Flash::FlashHash.new
+    flash.alert = I18n.t(env['omniauth.error.type'], scope: :omniauth)
+    SessionsController.action(:new).call(env)
+  end
 end
