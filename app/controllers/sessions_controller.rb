37b8b0b69ea473f3f4a9041f7c31ccefe53ce627
Julian Kniephoff <me@juliankniephoff.com>
2016-01-16 18:03:52 +0100

    Handle login using multiple identities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,63 @@ class SessionsController < ApplicationController
   end
 
   def create
+    authentication = Authentication.find_or_create_with_auth_hash(auth_hash)
+
+    # TODO Do these notices make sense in context?
+    # TODO This mixes computation with IO
+    notice = if signed_in?
+      if authentication.user == current_user
+        "Dieses #{provider}-Konto ist bereits mit Ihrem Konto verknüpft."
+      else # TODO We possibly steal a link to another account here
+        identity.update(user: current_user)
+        "Sie können sich von nun an mit diesem #{provider}-Konto anmelden."
+      end
+    else # TODO This could be shorter
+      if authentication.user.present?
+        'Sie wurden erfolgreich angemeldet.'
+      else
+        authentication.create_user(name: name)
+        authentication.save
+        'Es wurde ein neues Konto für Sie angelegt.'
+      end
+    end
+
+    # TODO This is actually only necessary, when we are not signed_in?
+    self.current_user = authentication.user
+
     # TODO Redirect to where we came from
-    redirect_to root_path
+    redirect_to root_path, notice: notice
   end
 
   def destroy
+    self.current_user = nil
+
     # TODO Redirect to where we came from
-    redirect_to login_path
+    redirect_to root_path, notice: 'Sie wurden erfolgreich abgemeldet.'
   end
 
   def failure
     # TODO Should we know about the env here?
     @message = t(env['omniauth.error.type'], scope: :omniauth)
     render :new
+  end
+
+  private
+
+  def auth_hash
+    env['omniauth.auth']
+  end
+
+  # TODO Should we pack information about the auth_hash in here?
+  # TODO Maybe we should not call this hash.
+  # We can also access its values using accessors
+  # plus its hashness is an implementation detail
+  def provider
+    # TODO Maybe we should translate :identity to something like "<seminar-manager>-Konto"
+    auth_hash[:provider].humanize
+  end
+
+  def name
+    auth_hash['info']['name']
   end
 end
