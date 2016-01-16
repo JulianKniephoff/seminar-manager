d36a70d5e98aa77323face5953603b1f59272180
Julian Kniephoff <me@juliankniephoff.com>
2016-01-16 18:03:24 +0100

    Add helper methods for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # TODO These belong in their own module?
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  helper_method :current_user, :signed_in?
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.nil? ? nil : user.id
+  end
 end
