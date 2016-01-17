ff31c6286f736d066a31c8b20e1564b618576496
Julian Kniephoff <me@juliankniephoff.com>
2016-01-17 14:20:08 +0100

    Fix comment relations

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,6 @@
 class Idea < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: :author_id
+  has_many :comments
 
   validates :title, uniqueness: true
 end
