d5a4f7a772188777c919aabba69221ad5e75833f
Julian Kniephoff <me@juliankniephoff.com>
2016-01-17 13:23:02 +0100

    Clean up comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,53 +1,40 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  # TODO The fact that this belongs to cancan is wrong
+  load_resource :idea
+  load_resource :comment, through: :idea, shallow: true
 
-  # GET /comments
-  # GET /comments.json
+  # GET /idea/1/comments
   def index
-    @comments = Comment.all
   end
 
   # GET /comments/1
-  # GET /comments/1.json
   def show
   end
 
-  # GET /comments/new
+  # GET /idea/1/comments/new
   def new
-    @comment = Comment.new
   end
 
   # GET /comments/1/edit
   def edit
   end
 
-  # POST /comments
-  # POST /comments.json
+  # POST /idea/1/comments
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to @comment, notice: 'Comment was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.update(comment_params)
+      redirect_to @comment, notice: 'Comment was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -55,17 +42,10 @@ class CommentsController < ApplicationController
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
