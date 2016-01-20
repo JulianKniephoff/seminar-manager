2caf706aa625d030194d37d488da81d53eaaae4b
Julian Kniephoff <me@juliankniephoff.com>
2016-01-20 21:22:00 +0100

    Associate idea and likes

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,7 @@
 class Idea < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   has_many :comments
+  has_many :likes, inverse_of: :idea
 
   validates :title, uniqueness: true
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ActiveRecord::Base
   belongs_to :user
-  belongs_to :idea
+  belongs_to :idea, inverse_of: :likes
 end
