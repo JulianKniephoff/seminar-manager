8cd97d5fa7929c0a07e2c94ed85ef9d0c7b121e4
Julian Kniephoff <me@juliankniephoff.com>
2016-01-20 20:22:22 +0100

    Add likes join model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117102952) do
+ActiveRecord::Schema.define(version: 20160120190613) do
 
   create_table "authentications", force: :cascade do |t|
     t.string   "provider"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 20160117102952) do
     t.datetime "updated_at",      null: false
     t.string   "name"
   end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["idea_id"], name: "index_likes_on_idea_id"
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
