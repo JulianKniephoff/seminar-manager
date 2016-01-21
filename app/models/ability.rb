232a9401a84eec6e61635e612654aa1977176beb
Julian Kniephoff <me@juliankniephoff.com>
2016-01-21 15:58:42 +0100

    Add a TODO

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,6 +1,9 @@
 class Ability
   include CanCan::Ability
 
+  # TODO Can we also say something like this:
+  #     can ..., user: user
+  # ?
   def initialize(user)
     if user.present?
       can :create, Idea
