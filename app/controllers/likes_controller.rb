0a6dc6d1a7306a96b21fefa66294cdd8e674d99a
Julian Kniephoff <me@juliankniephoff.com>
2016-01-21 12:17:49 +0100

    Refactor likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
-  load_resource :idea
-  load_and_authorize_resource :like, through: :idea, shallow: true
+  # TODO Shouldn't this be handled using AJAX?
+  # TODO Do we even need notices?
 
   # GET /idea/1/likes
   def index
@@ -8,9 +8,39 @@ class LikesController < ApplicationController
 
   # POST /idea/1/likes
   def create
+    # TODO Do we actually need a notice here?
+    # TODO Mixing computation and IO again ...
+    authorize like, :create
+    notice = if like.save
+               'Ihre Stimme wurde erfolgreich gespeichert.'
+             else
+               # TODO Actually check the error ...
+               'Sie haben für dieses Thema schon abgestimmt.'
+             end
+    redirect_to idea, notice: notice
   end
 
   # DELETE /likes/1
   def destroy
+    # TODO De we even want likes to be destroyable?
+    authorize like, :destroy
+    like.destroy
+    # TODO Redirect to where we came from
+    redirect_to idea, notice: 'Ihre Stimme wurde erfolgeich gelöscht.'
   end
+
+  private
+
+    # TODO DRY up the memoization?
+    def idea
+        @idea ||= Idea.find(params[:idea_id])
+    end
+
+    def likes
+      @likes ||= idea.likes
+    end
+
+    def like
+      @like ||= idea.like(current_user)
+    end
 end
