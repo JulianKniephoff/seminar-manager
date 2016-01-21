b8d6a773efe1e61a6be71c96e9bc24aeafbec3e8
Julian Kniephoff <me@juliankniephoff.com>
2016-01-21 16:03:25 +0100

    Like likes to users

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, inverse_of: :likes
   belongs_to :idea, inverse_of: :likes
 
   validates :user, uniqueness: { scope: :idea }
