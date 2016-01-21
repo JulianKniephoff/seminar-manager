47b3c90b7c5c4a4b2ea6d516191c82b66cb1fe35
Julian Kniephoff <me@juliankniephoff.com>
2016-01-21 16:03:52 +0100

    Add convenience methods for the liking system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_many :likes
+
+  # TODO Should this live here or in Idea?
+  # TODO Also the naming is confusing, considering Idea#like ...
+  def like(idea)
+    likes.where(idea: idea).first
+  end
+
+  # TODO Redundant?
+  def likes?(idea)
+    # TODO Does not work
+    likes.map(&:idea_id).include?(idea.id)
+  end
 end
