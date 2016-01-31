45668f1386bfd9b53b4ea912ac5d6747b0743b1a
Julian Kniephoff <me@juliankniephoff.com>
2016-01-31 15:04:06 +0100

    Add email address attribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120190613) do
+ActiveRecord::Schema.define(version: 20160131135815) do
 
   create_table "authentications", force: :cascade do |t|
     t.string   "provider"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160120190613) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
