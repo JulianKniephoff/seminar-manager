2682f9b00b3dc25f8350e00646b95f98ab87ded6
Julian Kniephoff <me@juliankniephoff.com>
2016-01-31 17:36:36 +0100

    Refactor the likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,49 +2,30 @@ class LikesController < ApplicationController
   # TODO Shouldn't this be handled using AJAX?
   # TODO Do we even need notices?
 
+  load_resource :idea
+  load_and_authorize_resource :like, through: :idea, shallow: true
+
   # GET /idea/1/likes
   def index
   end
 
   # POST /idea/1/likes
   def create
-    # TODO Do we actually need a notice here?
-    # TODO Mixing computation and IO again ...
-    authorize! :create, like
-    notice = if like.save
-               'Ihre Stimme wurde erfolgreich gespeichert.'
-             else
-               # TODO Actually check the error ...
-               'Sie haben für dieses Thema schon abgestimmt.'
-             end
-    redirect_to idea, notice: notice
+    @like.user = current_user
+    saved = @like.save
+    redirect_to @idea, notice: saved ?
+                         'Ihre Stimme wurde erfolgreich gespeichert.'
+                       :
+                         # TODO Actually check the error ...
+                         'Sie haben für dieses Thema schon abgestimmt.'
+    # TODO Also the latter case should never happen ...
   end
 
   # DELETE /likes/1
   def destroy
-    authorize! :destroy, like
-    idea = like.idea # TODO Ugly
-    like.destroy
+    @idea = @like.idea  # TODO Why do we need to do this?!
+    @like.destroy
     # TODO Redirect to where we came from
-    redirect_to idea, notice: 'Ihre Stimme wurde erfolgeich gelöscht.'
+    redirect_to @idea, notice: 'Ihre Stimme wurde erfolgeich gelöscht.'
   end
-
-  private
-
-    # TODO DRY up the memoization?
-    def idea
-      @idea ||= Idea.find(params[:idea_id])
-    end
-
-    def likes
-      @likes ||= idea.likes
-    end
-
-    def like
-      @like ||= if params[:idea_id]
-                  idea.like(current_user)
-                else
-                  Like.find(params[:id])
-                end
-    end
 end
