6e915446100626b4bae4523b3785503002cce0f9
Julian Kniephoff <me@juliankniephoff.com>
2016-02-07 18:09:53 +0100

    Create markdown helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def current_controller?(name)
     name.to_s == controller_name
   end
+
+  def markdown(text)
+    markdown_renderer.render(text).html_safe
+  end
+
+  private
+    def markdown_renderer
+      @markdown_renderer ||= Redcarpet::Markdown.new(
+        Redcarpet::Render::HTML
+      )
+    end
 end
