ad044913de7eac982c5e663196009cbca934b262
Julian Kniephoff <me@juliankniephoff.com>
2016-04-06 15:35:54 +0200

    Add validations for talks

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -4,6 +4,10 @@ class Talk < ActiveRecord::Base
 
   has_and_belongs_to_many :topics
 
+  validates :title,
+            presence: true
+  validates :description,
+            presence: true
   validates :date,
             presence: true,
             uniqueness: true
